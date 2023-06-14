6d398f6f5bcf386d27d71e874293fe1ba26d84bc
tgartland89 <thomas.gartland@gmail.com>
2023-06-14 11:43:08 -0600

    added notes

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,4 +1,6 @@
 def parrot(squawk="Squawk!"):
-    
     print(squawk)
+#  this will clear first two pytest errors, prints argument and returns passed argument  
+
+# adding the return clears the last two printing Squawk and returning Squawk if no argument is passed 
     return squawk
